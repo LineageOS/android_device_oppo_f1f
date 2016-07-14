6a439af093e9e38da9068aec3bfcda506d98af3c
Brinly Taylor <brinly@brinly.me>
2016-07-14 13:54:23 +0930

    f1f: F1f is actually a dual SIM device.
    * lets get it to work like one
    
    Change-Id: I4f4528bfa41c9c11e7adb7d71c632cdc6d82b1e0

--- device.mk
+++ device.mk
@@ -80,7 +80,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ro.telephony.default_network=9,9 \
+    ro.telephony.ril.config=simactivation,sim2gsmonly \
+    persist.radio.multisim.config=dsds
 
 # Wifi
 PRODUCT_COPY_FILES += \
