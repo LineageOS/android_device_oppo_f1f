64da8abc7dabbc731bf233479be20490d45a186e
Brinly Taylor <brinly@cyanogenmod.org>
2016-07-14 00:54:56 -0700

    f1f: remove loggy.sh
    Change-Id: Ie824023e5f6b44ab898864df39ca8d677d318d6c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,12 +17,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-# testing for logs
-include $(CLEAR_VARS)
-LOCAL_MODULE           := loggy.sh
-LOCAL_MODULE_TAGS      := optional eng
-LOCAL_MODULE_CLASS     := ETC
-LOCAL_SRC_FILES        := etc/loggy.sh
-LOCAL_MODULE_PATH      := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
